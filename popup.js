76c7b50d5df046e8e53c41b9afb96abd4e7b67a1
Tom Riley <tdriley@gmail.com>
2018-01-28 02:15:03 +0000

    Removed stuff nobody will want to use; added storage set/get

--- popup.js
+++ popup.js
@@ -3,8 +3,8 @@
 function onPopupOpen(){
 	//when the popup opens
 	console.log('Exkeleton popup opened');
-	callBgFunc('logIt', {sText: 'Exkeleton popup opened'});
-
+	callBgFunc('logIt', null, 'Exkeleton popup opened');
+	
 };
 function onPopupClose(){
 	//when the popup closes
